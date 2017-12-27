c3a065c60c08998a8fb0cd8e08d4d7af0b25a122
NicholasMacedo <nicholas.francis.macedo@gmail.com>
2017-12-27 17:08:13 -0500

    Stealing, Rounds and Basic Question Changing

--- js/index.js
+++ js/index.js
@@ -9,16 +9,31 @@ correctBuzzer.setAttribute('src', 'http://www.qwizx.com/gssfx/usa/ff-clang.wav')
 var wrongBuzzer = document.createElement('audio');
 wrongBuzzer.setAttribute('src', 'http://www.qwizx.com/gssfx/usa/ff-strike.wav');
 
-var question = 'Types of Cars';
-var numAnswers = 5
-var answers = ['a', "b", "c", "d", "e"];
-var scores = ["10","20","30","40","50"];
-var strikes = 0;
+var answeredQuestions = [];
+var numWrongAnswers = 0;
+var numCorrectAnswers = 0;
+var round = 1;
+
+$(document).ready(function(){
+  var questionNumber = Math.floor(Math.random() * 10)% allQuestions.length;
+  while ($.inArray(questionNumber, answeredQuestions) != -1) {
+    console.log("Skipped Question");
+    questionNumber = Math.floor(Math.random() * 10)% allQuestions.length;
+  }
+
+  console.log(questionNumber);
 
 
-$("#theQuestion").html("Question: " + question+" <br>Round Score: <span id=\"score\">0</span>");
+  var question = allQuestions[questionNumber].question;
+  var numAnswers = allQuestions[questionNumber].numAnswers;
+  var answers = allQuestions[questionNumber].answers;
+  var scores = allQuestions[questionNumber].scores;
+  var strikes = 0;
+  var familyTurn = sessionStorage.family1;
+
+$("#theQuestion").html("Round " + round + "  - Question: " + question+" <br>Round Score: <span id=\"score\">0</span>");
 $("#families").html("Family Feud: " + sessionStorage.family1 + " vs " + sessionStorage.family2);
-
+$("#answerLabel").html("<b>"+familyTurn+"</b> Family Answer:");
 $("#familyScores").html("Total Points: " + sessionStorage.family1 + ": " + sessionStorage.family1Score + "  |  " + sessionStorage.family2 + ": " + sessionStorage.family2Score );
 
 for (var i = 1; i < numAnswers+1; i++) {
@@ -27,13 +42,16 @@ for (var i = 1; i < numAnswers+1; i++) {
   $("#score"+String(i)).html(scores[i-1]);    
 } 
 
-
 $("#usrAnswer").submit(function() {
     var subAns = $('#usrAnswer').find('input[name="submittedAnswer"]').val().toLowerCase();
     if ($.inArray(subAns, answers) != -1) {
         console.log("Found Answer");
         var answerNum = $.inArray(subAns, answers) + 1;
         item("#" + String(answerNum));
+        numCorrectAnswers += 1;
+        if (numCorrectAnswers == answers.length) {
+
+        }
     } else {
         strikes = strikes + 1;
         wrongAnswer();
@@ -42,10 +60,18 @@ $("#usrAnswer").submit(function() {
     return false;
 });
 
-$(".multiplier").click(function() {
+function multiplier(multip) {
   $(".multiplier").removeClass("active");
-  $(this).addClass("active");
-});
+  $(multip).addClass("active");
+}
+
+if (round == 1) {
+  multiplier("#single");
+} else if (round == 2) {
+  multiplier("#double");
+} else {
+  multiplier("#triple");
+}
 
 // $("#flip-all").click(function() {
 //   $(".card").flip(true);
@@ -78,16 +104,37 @@ function item (card) {
 function wrongAnswer () {
   if (strikes == 1) {
     $("h1").prepend("<div id=\"red-x\">X</div>");
-    $("#singleStrike").html("<img src=\"strike.png\" width=\"100\" height=\"100\">");
+    $("#singleStrike").html("<img src=\"strike.png\" width=\"50\" height=\"50\">");
   } else if (strikes == 2) {
     $("h1").prepend("<div id=\"red-x\">X X</div>");
-    $("#doubleStrike").html("<img src=\"strike.png\" width=\"100\" height=\"100\">");
+    $("#doubleStrike").html("<img src=\"strike.png\" width=\"50\" height=\"50\">");
   } else {
     $("h1").prepend("<div id=\"red-x\">X X X</div>");
-    $("#trippleStrike").html("<img src=\"strike.png\" width=\"100\" height=\"100\">");
+    $("#trippleStrike").html("<img src=\"strike.png\" width=\"50\" height=\"50\">");
   }
   setTimeout(function() {
     $("#red-x").remove()
   }, 1000);
   wrongBuzzer.play();
+  numWrongAnswers += 1;
+  if (numWrongAnswers == 3) {
+    if (familyTurn == sessionStorage.family1) {
+        alert(sessionStorage.family2 + " can steal!");
+        familyTurn = sessionStorage.family2;
+        $("#answerLabel").html("<b>"+familyTurn+"</b> Family Answer:");
+    } else {
+        alert(sessionStorage.family1 + " can steal!");
+        familyTurn = sessionStorage.family1;
+        $("#answerLabel").html("<b>"+familyTurn+"</b> Family Answer:");
+    }
+  } else if (numWrongAnswers == 4){
+      if (familyTurn == sessionStorage.family1) {
+        sessionStorage.family2Score += parseInt($("#score").text())
+      } else {
+        sessionStorage.family1Score += parseInt($("#score").text())
+      }
+      $("#familyScores").html("Total Points: " + sessionStorage.family1 + ": " + sessionStorage.family1Score + "  |  " + sessionStorage.family2 + ": " + sessionStorage.family2Score );
+  }
 }
+
+});
---
+++ js/questions.js
@@ -0,0 +1,14 @@
+var allQuestions = [
+  {
+    question : 'Types of Cars',
+    numAnswers : 5,
+    answers : ['honda', "audi", "bmw", "volvo", "mini cooper"],
+    scores : ["55","20","10","9","6"]
+  },
+  {
+    question : 'Favourite Colour',
+    numAnswers : 7,
+    answers : ['red', "green", "blue", "yelow", "orange","indigo","magenta"],
+    scores : ["40","30","20","7","3"]
+  }
+]
